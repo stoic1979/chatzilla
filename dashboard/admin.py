3ce91339b397fdcef55a5eb05139658fe8dc1706
Navjot Singh <weavebytes@gmail.com>
2016-07-25 13:21:35 +0530

    migrations

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from dashboard.models import Room, Message
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'is_read', 'created_at')
+
+# registering models
+admin.site.register(Room)
+admin.site.register(Message, MessageAdmin)
