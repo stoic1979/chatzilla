c95ad38edd6c6f0f3d2a890d9cec077b2720baa2
Navjot Singh <weavebytes@gmail.com>
2016-07-25 15:02:15 +0530

    more serializers

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -1,8 +1,19 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from dashboard.models import Room, Message
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'url', 'username', 'email', 'is_staff')
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
