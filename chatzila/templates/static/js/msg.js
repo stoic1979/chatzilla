81d098ba7d15ac4a5320fbecf4fe13d1d70f996f
Navjot Singh <weavebytes@gmail.com>
2016-07-26 22:16:13 +0530

    identented js

--- chatzila/templates/static/js/msg.js
+++ chatzila/templates/static/js/msg.js
@@ -1,64 +1,64 @@
-		var scrollOffset = 100;
-		var div = document.getElementById("msgListDiv");
-		var scrollInit = false;		
-		var selectedUser = "";
-		
+var scrollOffset = 100;
+var div = document.getElementById("msgListDiv");
+var scrollInit = false;		
+var selectedUser = "";
+
 
 //dictionary for holding the all users messages
-		var usersMessages={};
+var usersMessages={};
 
 $(document).ready(function(){
-		$("#user-list li").bind("click", function() {
-			$("ul li").removeClass('focus-user');	
-	  		$(this).addClass('focus-user');	
-			selectedUser = $(this).text();
-			focusUser();
-		});
-	});
+    $("#user-list li").bind("click", function() {
+        $("ul li").removeClass('focus-user');	
+        $(this).addClass('focus-user');	
+        selectedUser = $(this).text();
+        focusUser();
+    });
+});
 
 
 function MessageData(user, ts, msg) {
-	this.user = user;
-	this.ts = ts;
-	this.msg = msg;
+    this.user = user;
+    this.ts = ts;
+    this.msg = msg;
 }
 
 
 //////////////////////////////////////////////////////
 //                                                  //
-// get the current date and time					//
+// get the current date and time	   	    //
 //                                                  //
 //////////////////////////////////////////////////////
 function getCurDateTime(){
-		var now = new Date();
-		var month =now.getMonth()+1;
-		month = month < 10 ? "0"+month : month;
-		var date = now.getDate();	
-		date = date < 10 ? "0"+date : date;
-		var year = now.getYear()+1900;
-		var hh = now.getHours();
-		hh = hh < 10 ? "0"+hh : hh > 12 ? "0"+(hh-12) : hh;  
-		var mm = now.getMinutes();
-		mm = mm < 10 ? "0"+mm : mm;  
-		var ss = now.getSeconds();
-		ss = ss < 10 ? "0"+ss : ss;  
+    var now = new Date();
+    var month =now.getMonth()+1;
+    month = month < 10 ? "0"+month : month;
+    var date = now.getDate();	
+    date = date < 10 ? "0"+date : date;
+    var year = now.getYear()+1900;
+    var hh = now.getHours();
+    hh = hh < 10 ? "0"+hh : hh > 12 ? "0"+(hh-12) : hh;  
+    var mm = now.getMinutes();
+    mm = mm < 10 ? "0"+mm : mm;  
+    var ss = now.getSeconds();
+    ss = ss < 10 ? "0"+ss : ss;  
 
-		var dateTime = date+"/"+ month +"/"+year+" :: "+hh+":"+mm+":"+ss;
-		return dateTime;
-	}
-		
+    var dateTime = date+"/"+ month +"/"+year+" :: "+hh+":"+mm+":"+ss;
+    return dateTime;
+}
+
 
 
 //////////////////////////////////////////////////////
 //                                                  //
-// function for initializing scrollbar			    //
+// function for initializing scrollbar		    //
 //                                                  //
 //////////////////////////////////////////////////////
 function initScroll(){
-		if(!scrollInit){
-				div.scrollTop += scrollOffset;				
-			}	
-		}
+    if(!scrollInit){
+        div.scrollTop += scrollOffset;				
+    }	
+}
 
 
 //////////////////////////////////////////////////////
@@ -67,37 +67,37 @@ function initScroll(){
 //                                                  //
 //////////////////////////////////////////////////////
 function getMessage(){
-		
-		var message = document.getElementById("msg");
-		var msgTxt = message.value;
-		message.value = "";		
-		return msgTxt;
-	}
+
+    var message = document.getElementById("msg");
+    var msgTxt = message.value;
+    message.value = "";		
+    return msgTxt;
+}
 
 //////////////////////////////////////////////////////
 //                                                  //
 // function for creating GUI for currently added    //
-// messege											//
-//			                                        //
+// messege				 	    //
+//			                            //
 //////////////////////////////////////////////////////
 
 function createMessageGUI(user, ts, message){
 
-			initScroll();
-			var usersMessList = document.getElementById("usersMessList");
-			var newRow = usersMessList.insertRow(-1);
-			var usr = newRow.insertCell(0);
-			var mess = newRow.insertCell(1);
+    initScroll();
+    var usersMessList = document.getElementById("usersMessList");
+    var newRow = usersMessList.insertRow(-1);
+    var usr = newRow.insertCell(0);
+    var mess = newRow.insertCell(1);
 
-			if(selectedUser!=""){
-			usr.innerHTML = '<p class="user">'+ user + '<span id=""> [ '+ ts +' ] </span> :</p>';
-			mess.innerHTML =  '<div class="message" style="float:left;"> <p class="mess-body">'+ message +' </p></div>';
-			moveScrollDown(div);
-			}
-			else{
-				alert("Please select a user");
-		}			
-	}
+    if(selectedUser!=""){
+        usr.innerHTML = '<p class="user">'+ user + '<span id=""> [ '+ ts +' ] </span> :</p>';
+        mess.innerHTML =  '<div class="message" style="float:left;"> <p class="mess-body">'+ message +' </p></div>';
+        moveScrollDown(div);
+    }
+    else{
+        alert("Please select a user");
+    }			
+}
 
 
 //////////////////////////////////////////////////////
@@ -107,20 +107,20 @@ function createMessageGUI(user, ts, message){
 //////////////////////////////////////////////////////
 
 function sendMessage(message ){
-		
-			var ts = getCurDateTime();
-			
-			var msg = new MessageData(selectedUser, ts, message);
-			if(selectedUser in usersMessages) {
-				var msgLst = usersMessages[selectedUser];
-				msgLst.push(msg);		
-			} else { // first time
-				var msgLst = [];
-				msgLst.push(msg);
-				usersMessages[selectedUser] = msgLst;
-			}
-			createMessageGUI(selectedUser, ts, message);			
-	}
+
+    var ts = getCurDateTime();
+
+    var msg = new MessageData(selectedUser, ts, message);
+    if(selectedUser in usersMessages) {
+        var msgLst = usersMessages[selectedUser];
+        msgLst.push(msg);		
+    } else { // first time
+        var msgLst = [];
+        msgLst.push(msg);
+        usersMessages[selectedUser] = msgLst;
+    }
+    createMessageGUI(selectedUser, ts, message);			
+}
 
 //////////////////////////////////////////////////////
 //                                                  //
@@ -129,8 +129,8 @@ function sendMessage(message ){
 //////////////////////////////////////////////////////
 
 function moveScrollDown(div){		
-		div.scrollTop +=scrollOffset;
-	}
+    div.scrollTop +=scrollOffset;
+}
 
 
 //////////////////////////////////////////////////////
@@ -139,15 +139,15 @@ function moveScrollDown(div){
 //                                                  //
 //////////////////////////////////////////////////////
 function focusUser(){
-		
-		// clear all messages from right side
-		$("#usersMessList tr").remove();
 
-		// show messages for selected user on right side
-		var msgLst = usersMessages[selectedUser];
-		if (! (selectedUser in usersMessages) ) return;
-		for(var i=0; i < msgLst.length; i++) {
-			var msg = msgLst[i];
-			createMessageGUI(msg.user, msg.ts, msg.msg);
-		}
+    // clear all messages from right side
+    $("#usersMessList tr").remove();
+
+    // show messages for selected user on right side
+    var msgLst = usersMessages[selectedUser];
+    if (! (selectedUser in usersMessages) ) return;
+    for(var i=0; i < msgLst.length; i++) {
+        var msg = msgLst[i];
+        createMessageGUI(msg.user, msg.ts, msg.msg);
+    }
 }
